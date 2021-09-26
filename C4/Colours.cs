e59306baad64d61003e0b94492c0484ca8377967
Grahame White <4whitg16@solent.ac.uk>
2021-09-26 20:57:34 +0100

    ditch the bitmapped images and just draw directly to the graphics surface

--- C4/Colours.cs
+++ C4/Colours.cs
@@ -1,17 +1,21 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using C4.LibC4;
 
 namespace C4.Gui
 {
-    class Colours
+    internal class Colours
     {
         public Color Light { get; }
         public Color Dark { get; }
+        public Color Outline { get; }
 
         public Colours(Token player)
         {
+            Outline = Color.Black;
             Light = player == Token.Player1 ? Color.Red : Color.Yellow;
             Dark = player == Token.Player1 ? Color.DarkRed : Color.Gold;
         }
+
     }
 }
--- C4/Renderer.cs
+++ C4/Renderer.cs
@@ -1,95 +1,61 @@
 ﻿using System;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Reflection;
 using C4.LibC4;
 
 namespace C4.Gui
 {
     internal class Renderer
     {
-        private Image GetImage(String imageName)
-        {
-            Assembly assembly = GetType().GetTypeInfo().Assembly;
-            Stream resource = assembly.GetManifestResourceStream($"C4.Gui.{imageName}");
-            if (resource == null) throw new InvalidOperationException();
-            using Image tmpImage = new Bitmap(resource);
-            Image image = new Bitmap(tmpImage.Width, tmpImage.Height, PixelFormat.Format32bppArgb);
-            using Graphics g = Graphics.FromImage(image);
-            g.DrawImage(tmpImage, new Rectangle(0, 0, image.Width, image.Height));
-
-            return image;
-        }
+        private const Single CELL_SIZE = 100;
+        private const Single TOKEN_SIZE = 80;
+        private const Single TOKEN_OUTER = 75;
+        private const Single TOKEN_INNER = 60;
 
         internal void PrepareMove(Graphics g, Int32 column, Token player)
         {
-            Image token = GetToken("token.png", new Colours(player));
-            g.Clear(Color.White);
-            g.DrawImage(token, new Rectangle(column * 100, 0, token.Width, token.Height));
-        }
-
-        private Image GetToken(String imageName, Colours colours)
-        {
-            Assembly assembly = GetType().GetTypeInfo().Assembly;
-            Stream resource = assembly.GetManifestResourceStream($"C4.Gui.{imageName}");
-            using Image tmpImage = new Bitmap(resource);
-
-            var colourMap = new ColorMap[2];
-            colourMap[0] = new ColorMap
-            {
-                OldColor = Color.FromArgb(255, 150, 150, 150),
-                NewColor = colours.Dark
-            };
-            colourMap[1] = new ColorMap
-            {
-                OldColor = Color.FromArgb(255, 200, 200, 200),
-                NewColor = colours.Light
-            };
-            var attr = new ImageAttributes();
-            attr.SetRemapTable(colourMap);
-
-            Image image = new Bitmap(tmpImage.Width, tmpImage.Height, PixelFormat.Format32bppArgb);
-            using Graphics g = Graphics.FromImage(image);
-            g.DrawImage(tmpImage, new Rectangle(0, 0, image.Width, image.Height), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, attr);
-
-            return image;
-        }
-
-        public Image DrawTokens(IGame game)
-        {
-            Image background = new Bitmap(700, 600);
-            using Graphics g = Graphics.FromImage(background);
-            g.Clear(Color.White);
-
-            for (var col = 0; col < game.Board.ColumnCount; col++)
-            {
-                for (var row = 0; row < game.Board.Columns[0].RowCount; row++)
-                {
-                    if (game.Board.Columns[col].Rows[row] == Token.None) continue;
-                    PlaceToken(g, col, row, game.Board.Columns[col].Rows[row]);
-                }
-            }
-
-            return background;
-        }
-
-        private void PlaceToken(Graphics g, Int32 col, Int32 row, Token player)
-        {
-            Int32 boardRow = 5 - row;
-            Image token = GetToken("token.png", new Colours(player));
-            g.DrawImage(token, new Rectangle(col * 100, boardRow * 100, token.Width, token.Height));
+            DrawToken(g, column, 0, player);
         }
 
         public void UpdateScreen(Graphics g, IGame game)
         {
-            Image background = DrawTokens(game);
-            Image board = GetImage("board.png");
+            // draw board
+            g.Clear(Color.Blue);
 
-            g.CompositingMode = CompositingMode.SourceOver;
-            g.DrawImage(background, 0, 0, background.Width, background.Height);
-            g.DrawImage(board, 0, 0, board.Width, board.Height);
+            // draw tokens
+            DrawTokens(g, game);
+        }
+
+        private static void DrawTokens(Graphics g, IGame game)
+        {
+            for (var col = 0; col < game.Board.ColumnCount; col++)
+            {
+                for (var row = 0; row < game.Board.Columns[0].RowCount; row++)
+                {
+                    if (game.Board.Columns[col].Rows[row] == Token.None)
+                    {
+                        DrawCircle(g, col, (5 - row), TOKEN_SIZE, Color.White);
+                    }
+                    else
+                    {
+                        DrawToken(g, col, (5 - row), game.Board.Columns[col].Rows[row]);
+                    }
+                }
+            }
+        }
+
+        private static void DrawToken(Graphics g, Int32 column, Int32 row, Token player)
+        {
+            var colours = new Colours(player);
+            DrawCircle(g, column, row, TOKEN_SIZE, colours.Outline);
+            DrawCircle(g, column, row, TOKEN_OUTER, colours.Dark);
+            DrawCircle(g, column, row, TOKEN_INNER, colours.Light);
+        }
+
+        private static void DrawCircle(Graphics g, Int32 column, Int32 row, Single diameter, Color colour)
+        {
+            Single top = (row * CELL_SIZE) + (CELL_SIZE - diameter) / 2;
+            Single left = (column * CELL_SIZE) +  (CELL_SIZE - diameter) / 2;
+            g.FillEllipse(new SolidBrush(colour), left, top, diameter, diameter);
         }
     }
 }
